2c8928f2863d27c1acf9595f40dbce6c4c8808be
QuantumTacoNinja <worapat1h@gmail.com>
2025-01-22 19:01:15 +0700

    Linear plot

---
+++ plot_pygame.py
@@ -0,0 +1,37 @@
+import pygame
+import math
+import random
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger()
+logger.info("Program started")
+
+pygame.init()
+
+w = 600
+h = 600
+
+screen = pygame.display.set_mode((w, h))
+pygame.display.set_caption("Draw graph")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+
+    screen.fill((0, 0, 0))
+
+    for x in range(w):
+        x = w - x
+        y = h - x
+        screen.set_at((x, y), (255, 255, 255))
+        # pygame.draw.line(screen, (0, 255, 0), (10, 10), (300, 300))
+
+    pygame.display.flip()
+
+pygame.quit()
